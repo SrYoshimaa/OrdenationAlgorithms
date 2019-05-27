cbf5a803a7a84fd67a921dea39607ad76a79eb77
Elivelton <eliveltonpw@live.com>
2019-05-27 19:37:05 -0300

    Create QuickSort.java

---
+++ src/sortings/QuickSort.java
@@ -0,0 +1,13 @@
+package sortings;
+
+import comparisons.Comparables;
+
+public class QuickSort<T> implements Sort <T>{
+
+    @Override
+    public long sort(T[] array, Comparables<T> comparables) {
+
+
+        return 0;
+    }
+}
