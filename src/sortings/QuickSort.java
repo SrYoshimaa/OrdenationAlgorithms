51e545e20afade034f37ae128c1d1ad85982574e
Elivelton <eliveltonpw@live.com>
2019-05-28 20:47:18 -0300

    Update QuickSort.java

--- src/sortings/QuickSort.java
+++ src/sortings/QuickSort.java
@@ -1,54 +1,47 @@
-/*package sortings;
+package sortings;
 
 import comparisons.Comparables;
+
 
 public class QuickSort<T> implements Sort <T>{
 
     @Override
-    public long sort(T[] array, Comparables<T> comparables) {
+    public long sort(T array[], Comparables<T> comparables) {
 
 		long start = System.currentTimeMillis();
-		
 
-			if (array == null || array.length == 0)
-				return start;
 
-			if (Comparables >= comparables)
-				return start;
 
-		// Define o Pivo
-		int middle = Comparables + (comparables - Comparables) / 2;
-		int pivot = array[middle];
-
-		// Define left < pivot e right > pivot
-		int i = Comparables, j = comparables;
-		while (i <= j) {
-			while (array[i] < pivot) {
-				i++;
-			}
-
-			while (array[j] > pivot) {
-				j--;
-			}
-
-			if (i <= j) {
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				i++;
-				j--;
-			}
-		}
-
-		// recursively sort two sub parts
-		if (Comparables < j)
-			quickSort(array, Comparables, j);
-
-		if (comparables > i)
-			quickSort(array, i, comparables);
 
 
         return System.currentTimeMillis() - start;
     }
+    public void quickSort(T array[], int p, int r){
+        if(p < r){
+//            q = Particao(T array[], int p, int r);
+        }
+
+
+
+    }
+
+    public long Particao(T array[], int p, int r, Comparables<T> comparables ){
+
+       T x = array[r];
+       int i = p -1;
+       T aux;
+
+       for(int j = p; j <= r - 1; j++){
+           if(comparables.compare(array[j], x) >= 0){
+               i = i + 1;
+               aux = array[i];
+               array[i] = array [j];
+               array[j] = aux;
+           }
+            aux = array[i + 1];
+           array[i + 1] = array[r];
+           array[r] = aux;
+       }
+       return i+1;
+    }
 }
-*/
